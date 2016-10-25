6fce50e54a859b5eedb71448262580e868130e3a
FilipeGood <filipegooddasilva@hotmail.com>
2016-10-25 21:01:02 +0100

    Create include.h

---
+++ include.h
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <time.h>
+#include <string.h>
+
+
+#define CONFIG_FILE_NAME "config.txt"
+
+
+
+
+//Ficheiro que contém os includes e variáveis necessárias
